f371bb5716e5f808553675ce9cee01fcff3129ed
gustavo ramon dos reis <gustavobilireis@hotmail.com>
2023-05-18 23:02:24 -0300

    add rotacao

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // eslint-disable-next-line quotes
+        espaco: "url('./src/images/background.png')",
+        animation: ['hover', 'focus'],
+      },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite', // Defina a duração e velocidade desejada
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
